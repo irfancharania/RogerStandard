df41a84e659f1fc6369e42aa1299a09b57c8feee
Irfan Charania <irfan.charania@gmail.com>
2017-09-24 18:39:08 -0700

    Testing ReleaseVersion

--- Sharpie/Script.fsx
+++ Sharpie/Script.fsx
@@ -1,4 +1,24 @@
 // Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
+#r @"C:\Users\irfan\Documents\GitHub\RogerStandard\packages\FSharpx.Collections.1.17.0\lib\net40\FSharpx.Collections.dll"
 
+
+#load "Changelog.DomainPrimitiveTypes.fs"
+#load "Changelog.Domain.fs"
+#load "Global.fs"
+
+
+open System
+open FSharpx.Collections
+open Changelog.DomainPrimitiveTypes
+open Changelog.Domain
+
+
+let a = VersionNumber.create(9);;
+let b = VersionNumber.create(100);;
+let c = VersionNumber.create(001);;
+let d = VersionNumber.create(230);;
+let e = VersionNumber.create(-2);;
+
+let f = Result.lift3 ReleaseVersion.create a d c
