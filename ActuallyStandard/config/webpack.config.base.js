bd988b2dd3b228402daaac6a29caf942fbfaac8f
Adam Bard <adam@adambard.com>
2017-12-03 15:57:05 -0800

    Prettify

--- ActuallyStandard/config/webpack.config.base.js
+++ ActuallyStandard/config/webpack.config.base.js
@@ -3,12 +3,12 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: [
         './Assets/src/index.tsx',
-        './Assets/sass/index.scss',
-        './node_modules/bootstrap/dist/css/bootstrap.min.css'
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './Assets/sass/index.scss'
     ],
     output: {
         filename: 'bundle.js',
-        path: __dirname + '/../wwwroot/dist'
+        path: __dirname + '/wwwroot/dist'
     },
 
     resolve: {
